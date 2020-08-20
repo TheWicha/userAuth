28fcc1eeceed6e7c5d487798a057d1cc9b5cbcd6
TheWicha <kontakt.danielwisniewski@gmail.com>
2020-08-20 16:28:32 +0200

    new style

--- src/components/Navbar/NavIcon.js
+++ src/components/Navbar/NavIcon.js
@@ -1,23 +1,19 @@
 import React from "react";
 import styled from "styled-components";
-import colorPalete from '../../helpers/colorPalete'
-import trim from '../../helpers/trim'
+import colorPalete from "../../helpers/colorPalete";
+import trim from "../../helpers/trim";
 
-const NavIcon = ({name}) => {
-  const StyledCon = styled.div`
-    background-color: ${colorPalete.white};
-    padding: 20px;
-  `;
-
-
-
+const NavIcon = ({ name }) => {
   return (
     <StyledCon>
-       <a href={`/${trim(name)}`}>
-            {name}
-       </a>
+      <a href={`/${trim(name)}`}>{name}</a>
     </StyledCon>
   );
 };
 
 export default NavIcon;
+
+const StyledCon = styled.div`
+  background-color: ${colorPalete.white};
+  padding: 20px;
+`;
