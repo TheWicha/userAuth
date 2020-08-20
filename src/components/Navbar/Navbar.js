cacd10fe2f9d162ea3341e4d27a5d28e503c5db9
TheWicha <kontakt.danielwisniewski@gmail.com>
2020-08-20 16:29:32 +0200

    change view depending if user or not and add new style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import styled from "styled-components";
 import colorPalete from "../../helpers/colorPalete";
@@ -7,28 +7,37 @@ import NavIcon from "./NavIcon";
 import HomeIcon from "./HomeIcon";
 
 const Navbar = () => {
-  const StyledCon = styled(Container)`
-    background-color: ${colorPalete.white};
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  `;
-
-  const NavIcons = styled.div`
-    display: flex;
-    justify-content: flex-end;
-  `;
+  const [user, setUser] = useState("guest");
 
   return (
     <StyledCon>
       <HomeIcon />
-      <NavIcons>
-        <NavIcon name={`Sign Up`} />
-        <NavIcon name={`Log In`} />
-        <NavIcon name={`Settings`} />
-      </NavIcons>
+      {user === "guest" && (
+        <NavIcons>
+          <NavIcon name={`Sign Up`} />
+          <NavIcon name={`Log In`} />
+        </NavIcons>
+      )}
+      {user !== "guest" && (
+        <NavIcons>
+          <NavIcon name={`Log Out`} />
+          <NavIcon name={`Settings`} />
+        </NavIcons>
+      )}
     </StyledCon>
   );
 };
 
 export default Navbar;
+
+const StyledCon = styled(Container)`
+  background-color: ${colorPalete.white};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NavIcons = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
